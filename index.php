da9edf594dcaae611e5da2725c34892e805a3543
kevinringier <kevinringier@outlook.com>
2017-11-30 18:36:33 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+define('TITLE', 'Home Page');
+include('templates/header.html');
+?>
+
+<form action="" method="post">
+	<p>Word: <input type="text" name="word"></p>
+	<p>
+		Type of Word:
+		<select name="type">
+			<option value="">Type</option>
+			<option value="adjective">Adjective</option>
+			<option value="noun">Noun</option>
+			<option value="verb">Verb</option>		
+		</select>
+	</p>	
+	<p>
+		<textarea rows="5" cols="80">Enter the definition here!</textarea>
+	</p>
+	<input type="submit" name="submit" value="Create Word!">
+</form>
+
+
+<?php
+include('templates/footer.html');
+?>
